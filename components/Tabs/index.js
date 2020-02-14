54a51a5e848665fd5db21c314857de41a14cee48
Justin Russell <Justin@Justins-MacBook.local>
2020-02-14 08:51:05 -0800

    initial commit

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,31 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+function newTab(item) {
+    //create element
+    const tab = document.createElement('div');
+
+    //add classes to element
+    tab.classList.add('tab');
+
+    //give content to element
+    tab.textContent = item;
+
+    return tab;
+}
+
+//create parent element to add newTab to
+const topicDiv = document.querySelector('.topics');
+
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+    .then(response => {
+        console.log(response.data.topics);
+        response.data.topics.forEach(topic => {
+            topicDiv.append(newTab(topic));
+        })
+    })
+    .catch(error => {
+        console.log('nope', error);
+    })
